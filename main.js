cc1bea01f9da728b302a88d8bfae1b4ee8681841
raman <khanahmadi.raman@yahoo.com>
2023-02-23 21:59:08 +0330

    finall

--- main.js
+++ main.js
@@ -6,12 +6,28 @@ let tempOutput = document.getElementById("temp");
 let windOutput = document.getElementById("wind");
 const apiKey = "c6907920fb744c4d3eeddf15e2a31be3";
 
-async function GetWether() {
-  let wetherResult = await (
-    await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${cityInput.value}&lon={lon}&appid=${apiKey}`
-    )
-  ).json();
+function convertToCel(value) {
+  return (value - 273).toFixed(2);
 }
 
-addInput.addEventListener("click", GetWether);
+async function GetWeather() {
+  let weatherResult = await (
+    await fetch(`https://api.openweathermap.org/data/2.5/weather?q=
+    ${cityInput.value}&appid=${apiKey}`)
+  ).json();
+  setInfo(weatherResult);
+}
+
+function setInfo(data) {
+  let cityName = data["name"];
+  let description = data["weather"][0]["description"];
+  let temp = data["main"]["temp"];
+  let wind = data["wind"]["speed"];
+
+  cityInput.innerHTML = `City :  ${cityName}`;
+  descOutput.innerHTML = `Description : ${description}`;
+  tempOutput.innerHTML = `Temprature : ${convertToCel(temp)}`;
+  windOutput.innerHTML = `Wind Speed ${wind} km/h`;
+}
+
+addInput.addEventListener("click", GetWeather);
